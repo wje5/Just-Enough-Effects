e4ea5ba34ebeb378e30ef2b8c3e9e00a17005cd2
wje5 <1834497651@qq.com>
2020-10-26 10:41:43 +0800

    update UI

--- src/main/java/com/pinball3d/effectinfo/Render.java
+++ src/main/java/com/pinball3d/effectinfo/Render.java
@@ -88,28 +88,66 @@ public class Render {
 		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
 		GlStateManager.translate(0, 0, 100F);
 		Gui.drawModalRectWithCustomSizedTexture(x, y, 0, 0, 120, 120, 256, 256);
-		Minecraft.getMinecraft().getTextureManager().bindTexture(INV);
+		FontRenderer fr = Minecraft.getMinecraft().fontRenderer;
+		int amp = effect.getAmplifier();
+		String[] desc = new String[5];
+		int max = 0;
+		for (int i = 0; i < 5; i++) {
+			String key = effect.getEffectName() + "." + i + ".desc";
+			if (I18n.hasKey(key)) {
+				desc[i] = I18n.format(key);
+				max = i;
+			}
+		}
+		int yOffset = y + 30;
+		int yOffsetTemp = 0;
+		for (int i = 0; i < 5; i++) {
+			String s = desc[i];
+			if (s != null) {
+				int yPos = yOffsetTemp == 0 ? yOffset : yOffsetTemp;
+				fr.drawSplitString(s, x + 30, yPos, 85, 0xFFFFFF);
+				if (yPos != y + 30) {
+					Gui.drawRect(x + 7, yPos - 5, x + 115, yPos - 4, 0xFF7F7F7F);
+					GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+				}
+				yOffsetTemp = 0;
+			} else if (yOffsetTemp == 0) {
+				yOffsetTemp = yOffset;
+			}
+			if (i <= max) {
+				Minecraft.getMinecraft().getTextureManager().bindTexture(TEXTURE);
+				Gui.drawModalRectWithCustomSizedTexture(x + 10, yOffset, 120, i * 16, 18, 16, 256, 256);
+			}
+			yOffset += s == null ? 20 : 25;
+		}
 		Potion potion = effect.getPotion();
 		if (potion.hasStatusIcon()) {
 			int i1 = potion.getStatusIconIndex();
+			Minecraft.getMinecraft().getTextureManager().bindTexture(INV);
 			Gui.drawModalRectWithCustomSizedTexture(x + 6, y + 7, 0 + i1 % 8 * 18, 198 + i1 / 8 * 18, 18, 18, 256, 256);
 		}
 		potion.renderInventoryEffect(effect, screen, x, y, 100.0F);
 		if (potion.shouldRenderInvText(effect)) {
 			String s1 = I18n.format(potion.getName());
-
-			if (effect.getAmplifier() == 1) {
+			if (amp == 1) {
 				s1 = s1 + " " + I18n.format("enchantment.level.2");
-			} else if (effect.getAmplifier() == 2) {
+			} else if (amp == 2) {
 				s1 = s1 + " " + I18n.format("enchantment.level.3");
-			} else if (effect.getAmplifier() == 3) {
+			} else if (amp == 3) {
 				s1 = s1 + " " + I18n.format("enchantment.level.4");
 			}
-			FontRenderer fr = Minecraft.getMinecraft().fontRenderer;
 			fr.drawStringWithShadow(s1, x + 28, y + 6, 0xFFFFFF);
 			String s = Potion.getPotionDurationString(effect, 1.0F);
 			fr.drawStringWithShadow(s, x + 28, y + 6 + 10, 0x7F7F7F);
 		}
+//		String s = I18n.format("enchantment.level.1");
+//		fr.drawStringWithShadow(s, x + 15 - fr.getStringWidth(s) / 2, y + 20, 0xFFFFFF);
+//		s = I18n.format("enchantment.level.2");
+//		fr.drawStringWithShadow(s, x + 15 - fr.getStringWidth(s) / 2, y + 27, 0xFFFFFF);
+//		s = I18n.format("enchantment.level.3");
+//		fr.drawStringWithShadow(s, x + 15 - fr.getStringWidth(s) / 2, y + 34, 0xFFFFFF);
+//		s = I18n.format("enchantment.level.4");
+//		fr.drawStringWithShadow(s, x + 15 - fr.getStringWidth(s) / 2, y + 41, 0xFFFFFF);
 		GlStateManager.popMatrix();
 	}
 }
