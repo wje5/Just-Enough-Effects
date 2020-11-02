4a8cbee517bb42407770aea2e9fd364ead788ceb
wje5 <1834497651@qq.com>
2020-11-02 17:05:48 +0800

    release

--- src/main/java/com/pinball3d/effectinfo/EffectInfo.java
+++ src/main/java/com/pinball3d/effectinfo/EffectInfo.java
@@ -5,6 +5,6 @@ import net.minecraftforge.fml.common.Mod;
 @Mod(modid = EffectInfo.MODID, name = EffectInfo.NAME, version = EffectInfo.VERSION)
 public class EffectInfo {
 	public static final String MODID = "effectinfo";
-	public static final String NAME = "Potion Effect Info";
+	public static final String NAME = "Just Enough Effects";
 	public static final String VERSION = "1.0.0";
 }
